53278de7411ff495ea1e93fcc9886f4c249819a4
4R1u <62248461+4R1u@users.noreply.github.com>
2025-09-08 10:37:02 +0500

    feat: render basic boards

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const Bot = require('./bot');
+const Gameboard = require('./gameboard');
+const Player = require('./player');
+const Ship = require('./ship');
+
+let player = new Player;
+let bot = new Bot;
+
+const boardRenderer = (function (doc) {
+    const drawBoard = function (num, player, own_view) {
+        const boardArea = doc.querySelector(`.board-${num}`);
+        const playerBoard = own_view ? player.own_view : player.opp_view;
+        for (let i = 0; i < 10; ++i)
+            for (let j = 0; j < 10; ++j) {
+                const littleSquare = doc.createElement("button");
+                littleSquare.type = "button";
+                littleSquare.textContent = playerBoard[i][j];
+                boardArea.appendChild(littleSquare);
+            }
+    };
+
+    return { drawBoard };
+})(document);
+
+boardRenderer.drawBoard(1, player, true);
+boardRenderer.drawBoard(2, bot, false);
